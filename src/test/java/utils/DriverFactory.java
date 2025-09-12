30d713ee15baba568d6a8b4cbce119b94fbaea4a
Safal Mehrotra <136926226+safal1216@users.noreply.github.com>
2025-09-12 17:13:55 +0530

    Create src/test/java/utils/DriverFactory.java

---
+++ src/test/java/utils/DriverFactory.java
@@ -0,0 +1,26 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DriverFactory {
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized");
+            driver = new ChromeDriver(options);
+        }
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
