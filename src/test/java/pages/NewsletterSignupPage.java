770d40b2e3fbe1be0bfcb345e714d2e525069fe6
Safal Mehrotra <136926226+safal1216@users.noreply.github.com>
2025-09-18 13:03:21 +0530

    Update src/test/java/pages/NewsletterSignupPage.java

--- src/test/java/pages/NewsletterSignupPage.java
+++ src/test/java/pages/NewsletterSignupPage.java
@@ -1,5 +1,30 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class NewsletterSignupPage {
-    // Page Object Model for Newsletter Signup Form
+    WebDriver driver;
+
+    public NewsletterSignupPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Locators
+    private By signupForm = By.id("newsletter-signup");
+    private By emailInput = By.id("email");
+    private By signupButton = By.id("signup-button");
+
+    // Methods
+    public boolean isSignupFormVisible() {
+        return driver.findElement(signupForm).isDisplayed();
+    }
+
+    public void enterEmail(String email) {
+        driver.findElement(emailInput).sendKeys(email);
+    }
+
+    public void clickSignupButton() {
+        driver.findElement(signupButton).click();
+    }
 }
