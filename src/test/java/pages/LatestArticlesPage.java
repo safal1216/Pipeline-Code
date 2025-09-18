24792fb04ee52ba8c10d2fafafde6c8b04266254
Safal Mehrotra <136926226+safal1216@users.noreply.github.com>
2025-09-18 13:03:19 +0530

    Update src/test/java/pages/LatestArticlesPage.java

--- src/test/java/pages/LatestArticlesPage.java
+++ src/test/java/pages/LatestArticlesPage.java
@@ -1,5 +1,31 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import java.util.List;
+
 public class LatestArticlesPage {
-    // Page Object Model for Latest Articles and Collections
+    WebDriver driver;
+
+    public LatestArticlesPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Locators
+    private By latestArticlesSection = By.id("latest-articles");
+    private By articleItems = By.cssSelector("#latest-articles .article-item");
+
+    // Methods
+    public boolean isLatestArticlesSectionVisible() {
+        return driver.findElement(latestArticlesSection).isDisplayed();
+    }
+
+    public List<WebElement> getArticleItems() {
+        return driver.findElements(articleItems);
+    }
+
+    public void clickArticleItem(int index) {
+        getArticleItems().get(index).click();
+    }
 }
