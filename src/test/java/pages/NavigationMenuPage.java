2aab244a1507c32837cb20de2886ddd5141c8fb4
Safal Mehrotra <136926226+safal1216@users.noreply.github.com>
2025-09-18 13:03:17 +0530

    Update src/test/java/pages/NavigationMenuPage.java

--- src/test/java/pages/NavigationMenuPage.java
+++ src/test/java/pages/NavigationMenuPage.java
@@ -1,5 +1,36 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import java.util.List;
+
 public class NavigationMenuPage {
-    // Page Object Model for Navigation Menu
+    WebDriver driver;
+
+    public NavigationMenuPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Locators
+    private By navigationMenu = By.id("nav-menu");
+    private By menuItems = By.cssSelector("#nav-menu a");
+
+    // Methods
+    public boolean isNavigationMenuVisible() {
+        return driver.findElement(navigationMenu).isDisplayed();
+    }
+
+    public List<WebElement> getMenuItems() {
+        return driver.findElements(menuItems);
+    }
+
+    public void clickMenuItem(String itemName) {
+        for (WebElement item : getMenuItems()) {
+            if (item.getText().equalsIgnoreCase(itemName)) {
+                item.click();
+                break;
+            }
+        }
+    }
 }
